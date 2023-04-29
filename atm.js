ee9f32233b6c411a2791f7933a4a1ae94092c321
Gulkhanim <guluhasanzade7@gmail.com>
2023-04-29 12:08:25 +0400

    update

--- atm.js
+++ atm.js
@@ -1,25 +1,82 @@
+
 var atmInput = document.querySelector ('#atmInput')
 var atmCashBackButton = document.querySelector ('#atmDeleteButton');
 var atmAddButton = document.querySelector ('#atmAddButton');
-// var atmShowButton = document.querySelector ('#atmShowButton');
 var atmTotal = document.querySelector("#atmTotal");
-var atmTotal = document.querySelector("#atmRemove");
+
+
+console.log(atmInput.value);
+
+var balance = 0;
+
+    function calc2() {
+        var addMoney = +atmInput.value
+        balance = balance + addMoney
+        atmTotal.innerHTML = `${balance} azn`
+        console.log(addMoney);
+    }
+    function calc1() {
+        var spendingMoney = +atmInput.value
+
+      if(balance <  spendingMoney) 
+      return;
+
+        balance =balance - spendingMoney;
+        atmTotal.innerHTML = `${balance} Azn`;
+    }
+    
+    console.log(calc1())
+
+atmAddButton.addEventListener('click',calc2 );
+atmCashBackButton.addEventListener('click', calc1)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var atmInput = document.querySelector ('#atmInput')
+// var atmCashBackButton = document.querySelector ('#atmDeleteButton');
+// var atmAddButton = document.querySelector ('#atmAddButton');
+// var atmShowButton = document.querySelector ('#atmShowButton');
+// var atmTotal = document.querySelector("#atmTotal");
+// var atmTotal = document.querySelector("#atmRemove");
 // var div = document.querySelector("div");
 
 
-// function BankCustomur(add, cashBack, show)  {
 
-//     this.add = add;
-//     this.cashBack = cashBack;
-//     this.show = show;
+// var arr = [];
 
-
-    
-// }
-
-var arr = [];
-
-console.log(atmInput.value);
+// console.log(atmInput.value);
 // console.log(atmCashBackButton);
 // console.log(atmAddButton);
 // console.log(atmRemove);
@@ -28,44 +85,46 @@ console.log(atmInput.value);
 
 
 
-var total;
-var balance ;
+// var total;
+// var balance ;
 
-function cash(a, b) {
-    return a-b;
-    }
+// function cash(a, b) {
+//     return a-b;
+//     }
 
-    function calc1() {
-         var spendingMoney = +atmInput.value
-        console.log(arr)
-        balance = total - spendingMoney;
-        atmTotal.innerHTML = `${balance} Azn`;
+//     function calc1() {
+//          var spendingMoney = +atmInput.value
+//         console.log(arr)
+//         balance = balance - spendingMoney;
+//         atmTotal.innerHTML = `${balance} Azn`;
     
         
     
     
-    }
+//     }
 
-    // atmCashBackButton.addEventListener('click',calc1 );
-    console.log(calc1())
+//     console.log(calc1())
 
-function add(a,b) {
-return a+b;
-}
+// function add(a,b) {
+// return a+b;
+// }
 
-function calc2() {
-    arr.push(+atmInput.value)
-    total = arr.reduce(add, 0)
-    atmTotal.innerHTML = `${total} azn`
+// function calc2() {
+//     arr.push(+atmInput.value)
+//     total = arr.reduce(add, 0)
+//     atmTotal.innerHTML = `${total} Azn`
 
     
 
-console.log(arr)
-}
+// console.log(arr)
+// }
 
-atmAddButton.addEventListener('click',calc2 );
+// atmAddButton.addEventListener('click',calc2 );
 
-atmCashBackButton.addEventListener('click', calc1)
+// atmCashBackButton.addEventListener('click', calc1)
 
 // console.log(atmCashBackButton)
+
+
+
 
